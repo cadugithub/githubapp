540d8fe80498bc831babe53c24e25917b8868b6e
cadugithub <carlosk2k2@gmail.com>
2019-09-24 16:19:00 -0300

    marcacao da pagina

--- webpack/src/app.js
+++ webpack/src/app.js
@@ -5,8 +5,39 @@ import React, { Component } from 'react'
 const App = () => 
 (
 
-    <div>
+    <div className='app'>
+        <div className='search'>
+            <input type='search' placeholder='digite o nome do usuário no github'/>
+        </div>
+        <div className='user-info'>
+            <img src='https://avatars2.githubusercontent.com/u/37313040?v=4'/>
+            <h1><a href='https://github.com/cadugithub' target='blank_'>Carlos Eduardo</a></h1>
         
+            <ul className='repos-info'>
+                <li>Repositórios: 7</li>
+                <li>Seguidores: 5</li>
+                <li>Seguindo: 5</li>
+            </ul>
+
+            <div className='actions'>
+                <button>ver repositórios</button>
+                <button>Ver favoritos</button>
+            </div>
+
+            <div className='repos'>
+                <h2>Repositórios:</h2>
+                <ul>
+                    <li><a href='#'>Nome do repositório</a></li>
+                </ul>
+            </div>
+
+            <div className='starred'>
+                <h2>Favoritos:</h2>
+                <ul>
+                    <li><a href='#'>Nome do repositório</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
 )
 export default App
